7634159c7ba6772084a624c65e5a94e161324a5b
BlackiCrystallik <98474798+BlackiCrystallik@users.noreply.github.com>
2022-02-16 20:21:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var slideIndex = 0;
+showSlides();
+
+function currentSlide(n) {
+    var dot = document.getElementsByClassName("dot");
+    showSlides2(slideIndex = n);
+    dot[n - 1].className = dots[n - 1].className.replace(" active", "");
+
+}
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 5000);
+}
+
+function showSlides2() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+}
